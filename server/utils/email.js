6422cd7eaea980b62c81c2ceb4432580b422aff2
musab-18 <musabiftikhar12@icloud.com>
2026-03-06 23:21:30 +0500

    fix: explicit ipv4 host for nodemailer to avoid ENETUNREACH in render

--- server/utils/email.js
+++ server/utils/email.js
@@ -1,7 +1,7 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
+    host: 'smtp.ipv4.gmail.com',
     port: 465,
     secure: true,
     auth: {
